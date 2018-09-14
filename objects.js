96b5918e3004371f548fa7675da4d51a76980ede
Paul Clark <clark.paul123@gmail.com>
2018-09-14 20:40:00 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,11 @@ var playlist = {
   ledZeppelin: 'Stairway to Heaven'
 };
 
+function updatePlaylist(playlist, artist, song) {
+  playlist[artist] = song;
+  return playlist;
+}
+
+function removeFromPlaylist(playlist, artist) {
+  delete playlist.artist;
+}
